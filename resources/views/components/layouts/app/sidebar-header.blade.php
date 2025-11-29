255b25534d94ea0ba5e31bd1bc04ffd55fd7e496
wildanal2 <40136374+wildanal2@users.noreply.github.com>
2025-11-29 14:36:42 +0700

    fix: remove unnecessary wire:navigate directive from daily report list item in sidebar - Updated the sidebar header to remove the wire:navigate directive from the "Data List Participant" navbar item, streamlining the navigation functionality.

--- resources/views/components/layouts/app/sidebar-header.blade.php
+++ resources/views/components/layouts/app/sidebar-header.blade.php
@@ -220,7 +220,7 @@
 
         <flux:navbar scrollable>
             <flux:navbar.item href="{{ route('admin.daily-report') }}" :current="request()->routeIs('admin.daily-report')" wire:navigate>Gallery Report</flux:navbar.item>
-            <flux:navbar.item href="{{ route('admin.daily-report-list') }}" :current="request()->routeIs('admin.daily-report-list')" wire:navigate>Data List Participant</flux:navbar.item>
+            <flux:navbar.item href="{{ route('admin.daily-report-list') }}" :current="request()->routeIs('admin.daily-report-list')" >Data List Participant</flux:navbar.item>
         </flux:navbar>
     </flux:header>
 
